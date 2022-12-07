8817b2ac8a49fe83586997884fad6969651e4d5f
MarcosVicencio <marcos.vicencio@alumnos.udg.mx>
2022-12-07 15:45:20 -0600

    Actualización del proyecto 07/12/2022

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 //Buscador tabla
-jQuery("#buscador").keyup(function(){
-    if( jQuery(this).val() != ""){
+jQuery("#buscador").keyup(function () {
+    if (jQuery(this).val() != "") {
         jQuery("#tablaLaWebera tbody>tr").hide();
         jQuery("#tablaLaWebera td:contiene-palabra('" + jQuery(this).val() + "')").parent("tr").show();
     }
-    else{
+    else {
         jQuery("#tablaLaWebera tbody>tr").show();
     }
 });
- 
-jQuery.extend(jQuery.expr[":"], 
-{
-    "contiene-palabra": function(elem, i, match, array) {
-        return (elem.textContent || elem.innerText || jQuery(elem).text() || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
-    }
-});
+
+jQuery.extend(jQuery.expr[":"],
+    {
+        "contiene-palabra": function (elem, i, match, array) {
+            return (elem.textContent || elem.innerText || jQuery(elem).text() || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+        }
+    });
 
 //Buscador de cartas de los paises
-$(document).ready(function(){
-    $('#buscador-pais').keyup(function(){
-       var nombres = $('.card-title');
-       var buscando = $(this).val();
-       var item='';
-       for( var i = 0; i < nombres.length; i++ ){
-           item = $(nombres[i]).html().toLowerCase();
-            for(var x = 0; x < item.length; x++ ){
-                if( buscando.length == 0 || item.indexOf( buscando ) > -1 ){
-                    $(nombres[i]).parents('.card').show(); 
-                }else{
-                     $(nombres[i]).parents('.card').hide();
+$(document).ready(function () {
+    $('#buscador-pais').keyup(function () {
+        var nombres = $('.card-title');
+        var buscando = $(this).val();
+        var item = '';
+        for (var i = 0; i < nombres.length; i++) {
+            item = $(nombres[i]).html().toLowerCase();
+            for (var x = 0; x < item.length; x++) {
+                if (buscando.length == 0 || item.indexOf(buscando) > -1) {
+                    $(nombres[i]).parents('.card').show();
+                } else {
+                    $(nombres[i]).parents('.card').hide();
                 }
             }
-       }
+        }
     });
-  });
+});
